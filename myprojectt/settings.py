a3fab951586a25fa85f96ddcc5b0e3b0dca491de
ichrakjawadi99 <ichrakjawadyy@gmail.commm>
2024-12-20 14:51:10 +0100

    Update settings.py

--- myprojectt/settings.py
+++ myprojectt/settings.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sites',  # Nécessaire pour django-allauth
+    'django.contrib.sites',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
